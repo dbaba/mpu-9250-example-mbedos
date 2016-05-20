312d41a81fb0bad6334c970eac89d2958bb416b6
Daisuke Baba <baba.daisuke@gmail.com>
2016-05-20 20:44:46 +0900

    Implement accelgyrocalMPU9250Async()

--- source/motion_async.cpp
+++ source/motion_async.cpp
@@ -7,12 +7,23 @@ static mbed::drivers::v2::I2C i2cAsyncBus(PB_9, PB_8);
 static MPU9250Async* motion_sensor;
 
 void mpu9250_async_task(void) {
+    printf("%s\r\n", "OK. mpu9250_async_task() started!");
+}
+
+static void mpu9250_async_task_who_am_i(void) {
     motion_sensor->whoAmI1([](i2c_async::StatusCode status, uint8_t data) {
         if (status == i2c_async::OK && data == 0x71) {
             printf("%s\r\n", "MPU-9250 WHO_AM_I is SUCCESS!!!");
+            motion_sensor->initAllAsync([](i2c_async::StatusCode status) {
+                if (status == i2c_async::OK) {
+                    minar::Scheduler::postCallback(mpu9250_async_task);
+                } else {
+                    printf("%s\r\n", "Error. Something wrong");
+                }
+            });
         } else {
             printf("%s status:%d\r\n", "MPU-9250 WHO_AM_I is FAAAAAAAAAIL....", status);
-            minar::Scheduler::postCallback(mpu9250_async_task).delay(minar::milliseconds(100));
+            minar::Scheduler::postCallback(mpu9250_async_task_who_am_i).delay(minar::milliseconds(100));
         }
     });
 }
@@ -20,11 +31,5 @@ void mpu9250_async_task(void) {
 void mpu9250_async_task_init(void) {
     i2cAsyncBus.frequency(400000);
     motion_sensor = new MPU9250Async(&i2cAsyncBus, 1);
-    motion_sensor->initAllAsync([](i2c_async::StatusCode status) {
-        if (status == i2c_async::OK) {
-            minar::Scheduler::postCallback(mpu9250_async_task);
-        } else {
-            printf("%s\r\n", "Error. Something wrong");
-        }
-    });
+    mpu9250_async_task_who_am_i();
 }
